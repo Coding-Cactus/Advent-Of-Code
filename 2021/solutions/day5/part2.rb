72130f069d9e1e82eb678f88ca350bace7e07861
Coding-Cactus <CodingCactus.cc@gmail.com>
2021-12-05 09:07:59 +0000

    Day 5

---
+++ 2021/solutions/day5/part2.rb
@@ -0,0 +1,21 @@
+points_covered = Hash.new(0)
+
+File.foreach("../../inputs/day5.txt") do |line|
+	start_coord, end_coord = line.split(" -> ")
+	start_x, start_y = start_coord.split(",").map { |n| n.to_i }
+	end_x, end_y = end_coord.split(",").map { |n| n.to_i }
+
+	current_x, current_y = start_x, start_y
+	direction_x = current_x < end_x ? 1 : -1
+	direction_y = current_y < end_y ? 1 : -1
+
+	points_covered["#{current_x},#{current_y}"] += 1
+	until current_x == end_x && current_y == end_y
+		current_x += direction_x if current_x != end_x
+		current_y += direction_y if current_y != end_y
+
+		points_covered["#{current_x},#{current_y}"] += 1
+	end
+end
+
+puts points_covered.reduce(0) { |sum, (_, num)| num >= 2 ? sum + 1 : sum }
