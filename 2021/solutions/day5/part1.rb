226e2b1b8579d87b42b4f5187a019eec86b467b1
Coding-Cactus <CodingCactus.cc@gmail.com>
2021-12-06 17:07:45 +0000

    Day 5

--- 2021/solutions/day5/part1.rb
+++ 2021/solutions/day5/part1.rb
@@ -8,13 +8,11 @@ File.foreach("../../inputs/day5.txt") do |line|
 	next if start_x != end_x && start_y != end_y
 
 	current_x, current_y = start_x, start_y
-	direction_x = current_x < end_x ? 1 : -1
-	direction_y = current_y < end_y ? 1 : -1
 
 	points_covered["#{current_x},#{current_y}"] += 1
 	until current_x == end_x && current_y == end_y
-		current_x += direction_x if current_x != end_x
-		current_y += direction_y if current_y != end_y
+		current_x += end_x <=> current_x
+		current_y += end_y <=> current_y
 
 		points_covered["#{current_x},#{current_y}"] += 1
 	end
