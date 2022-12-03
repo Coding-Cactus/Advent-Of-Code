0fd21e314c439efe3b00391160e12c6a322f8393
Coding-Cactus <codingcactus.cc@gmail.com>
2022-12-03 08:52:15 +0000

    :sparkles: day 3

---
+++ 2022/solutions/day3/part1.rb
@@ -0,0 +1,13 @@
+sum = 0
+
+File.foreach("../../inputs/day3.txt")do |line|
+  comp1, comp2 = line.strip.split("").each_slice(line.size/2).to_a
+  c = (comp1 & comp2)[0]
+
+  priority = c.ord - 96
+  priority += 58 if priority < 1
+
+  sum += priority
+end
+
+puts sum
