869009bee5dd7b572f1ab61364f336617b7a9d76
Coding-Cactus <codingcactus.cc@gmail.com>
2022-12-04 09:04:37 +0000

    :sparkles: day 4

---
+++ 2022/solutions/day4/part1.rb
@@ -0,0 +1,12 @@
+count = 0
+
+File.foreach("../../inputs/day4.txt") do |line|
+  pair1, pair2 = line.strip.split(",")
+
+  num1, num2 = pair1.split("-").map(&:to_i)
+  num3, num4 = pair2.split("-").map(&:to_i)
+
+  count += 1 if (num1 <= num3 && num2 >= num4) || (num1 >= num3 && num2 <= num4)
+end
+
+puts count
