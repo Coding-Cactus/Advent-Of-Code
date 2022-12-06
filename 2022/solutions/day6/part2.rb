80e7cc3121b8868eca9a619875a182d3dc96519c
Coding Cactus <65423356+Coding-Cactus@users.noreply.github.com>
2022-12-06 08:18:18 +0000

    ✨ day 6

---
+++ 2022/solutions/day6/part2.rb
@@ -0,0 +1,13 @@
+data = File.read("../../inputs/day6.txt").split("")
+
+window = Array::new(14) { "" }
+
+data.each_with_index do |c, i|
+    window.delete_at(0)
+    window << c
+
+    if window.all? { |l| l != "" && window.count(l) == 1 }
+        puts i + 1
+        break
+    end
+end
