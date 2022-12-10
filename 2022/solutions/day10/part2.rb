5cbc6ca8a9cf8dc0c3e6426b4e6fdbc20f407748
Coding-Cactus <codingcactus.cc@gmail.com>
2022-12-10 15:45:06 +0000

    :sparkles: day 10

---
+++ 2022/solutions/day10/part2.rb
@@ -0,0 +1,18 @@
+screen = []
+cycle = 0
+x = 1
+
+File.foreach("../../inputs/day10.txt") do |line|
+    op, num = line.strip.split
+
+    (1..2).each do |n|
+        if n == 1 || op == "addx"
+          cycle += 1
+          screen << ((x - (cycle - 1) % 40).abs > 1 ? "  " : "██")
+        end
+    end
+
+    x += num.to_i if op == "addx"
+end
+
+puts screen.each_slice(40).map { |row| row.join }
