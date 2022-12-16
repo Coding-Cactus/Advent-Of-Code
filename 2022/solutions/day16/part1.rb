9463a21ef25de2209a3840d78630e40a52c26c00
Coding-Cactus <codingcactus.cc@gmail.com>
2022-12-16 12:29:47 +0000

    :sparkles: day 16

---
+++ 2022/solutions/day16/part1.rb
@@ -0,0 +1,62 @@
+flows = {}
+connections = {}
+File.foreach("../../inputs/day16.txt") do |line|
+    valve, cons = line.strip.split("; ")
+
+    v = valve.split[1]
+    flows[valve.split[1]] = valve.split("=")[-1].to_i
+    connections[v] = cons.split[4..-1].join.split(",")
+end
+
+distances = {}
+flows.each do |current, _|
+    visits = connections.map { |v, _| [v, 9999999999999] }.to_h
+    paths = [current]
+    visits[current] = 0
+
+    until paths.length == 0
+        new_paths = []
+        paths.each do |path|
+            connections[path].each do |v|
+                if visits[path] + 1 < visits[v]
+                    new_paths << v
+                    visits[v] = visits[path] + 1
+                end
+            end
+        end
+        paths = new_paths
+    end
+
+    distances[current] = visits
+end
+
+distances = distances.map { |c, d| [c, d.reject { |l, _| flows[l] == 0 || c == l }] }.to_h
+flows = flows.reject { |_, p| p == 0 }
+
+best = 0
+states = [[["AA"], 30, 0]]
+until states.length == 0
+    new_states = []
+    states.each do |seen, mins, total|
+        if seen.length - 1 == flows.length
+            best = [total, best].max
+            next
+        end
+
+        current = seen[-1]
+        visits = distances[current]
+
+        visits.each do |valve, dist|
+            new_min = mins - dist - 1
+            if !seen.include?(valve) && new_min >= 0
+                t = total + new_min * flows[valve]
+                best = [t, best].max
+                new_states << [seen + [valve], new_min, t]
+            end
+        end
+    end
+
+    states = new_states
+end
+
+puts best
