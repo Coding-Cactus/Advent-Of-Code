68a1f6084226a070cae2cdb28e239a93d219ad43
Coding-Cactus <codingcactus.cc@gmail.com>
2023-11-30 00:44:06 +0000

    :bug: make runner work for all years

--- main.rb
+++ main.rb
@@ -1,32 +1,30 @@
-type = nil
+type = year = day = part = nil
+
 until %w[1 2].include?(type)
     print "\e[0m1. Run specific program\n2. Run latest program\nEnter 1/2: \e[36;3m"
     type = gets.chomp
 end
 
-year = day = part = nil
-
 case type
 when "1"
-	until ("2020".."2022").to_a.include?(year)
-		print "\e[0mEnter year: \e[36;3m"
-		year = gets.chomp
-	end
+  until Dir["*/"].include?("#{year}/")
+    print "\e[0mEnter year: \e[36;3m"
+    year = gets.chomp
+  end
 
-	until Dir["#{year}/*"].include?("#{year}/day#{day}")
-		print "\e[0mEnter day: \e[36;3m"
-		day = gets.chomp
-		day = "0" + day if day.length == 1
-	end
+  until Dir["#{year}/*"].include?("#{year}/day#{day}")
+    print "\e[0mEnter day: \e[36;3m"
+    day = "%02d" % gets.chomp
+  end
 
-	until %w[1 2].include?(part)
-		print "\e[0mEnter part: \e[36;3m"
-		part = gets.chomp
-    end
+  until %w[1 2].include?(part)
+    print "\e[0mEnter part: \e[36;3m"
+    part = gets.chomp
+  end
 when "2"
-    year = Dir["*"].reduce(0) { |max, c| File.directory?(c) ? [c.to_i, max].max : max }
-	day  = Dir["#{year}/*"].reduce(0) { |max, c| [c.split("/")[1][-2..-1].to_i, max].max }
-    part = File.exists?("#{year}/day#{day}/part2.rb") ? 2 : 1
+  year = Dir["*/"].reduce(0) { |max, c| [c.to_i, max].max }
+  day  = "%02d" % Dir["#{year}/*"].reduce(0) { |max, c| [c.split("/")[1][-2..-1].to_i, max].max }
+  part = File.exists?("#{year}/day#{day}/part2.rb") ? 2 : 1
 end
 
 Dir.chdir("#{year}/day#{day}")
