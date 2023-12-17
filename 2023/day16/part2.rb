2f502a9b6214959249d6ddb510e35fe57f946a64
Coding-Cactus <codingcactus.cc@gmail.com>
2023-12-17 14:21:37 +0000

    :zap: improve day 16 part 2

--- 2023/day16/part2.rb
+++ 2023/day16/part2.rb
@@ -16,41 +16,38 @@ next_moves = {
 
 max = 0
 
+# start from edge: left, top, right, bottom
 (0..3).each do |edge|
-  start_x = [0, grid[0].length - 1, 0, 0][edge]
-  start_y = [0, 0, grid.length - 1, 0][edge]
+  start_x = edge == 2 ? grid[0].length - 1 : 0
+  start_y = edge == 3 ? grid.length - 1 : 0
 
   while start_x.between?(0, grid[0].length - 1) && start_y.between?(0, grid.length - 1)
-    start_coord = [start_x, start_y]
-    start_direction = (edge + 1) % 4
 
-    energised = Set[start_coord]
-    visited = Set[[start_coord, start_direction]]
-    current = Set[[start_coord, start_direction]]
+    energised = Set[[start_x, start_y]]
+    current   = [[start_x, start_y, edge]]    # [x, y, direction][]
+    visited   = Set[[start_x, start_y, edge]]
 
     until current.empty?
-      next_cur = Set[]
+      x, y, direction = current.pop
+      next_moves[grid[y][x]][direction].each do |new_d|
+        dx, dy = directions[new_d]
+        _x, _y = x + dx, y + dy
 
-      current.each do |(x, y), direction|
-        next_moves[grid[y][x]][direction].each do |new_d|
-          dx, dy = directions[new_d]
-          _x, _y = x + dx, y + dy
+        next unless _x.between?(0, grid[0].length - 1) && _y.between?(0, grid.length - 1)
 
-          next unless _x.between?(0, grid[0].length - 1) && _y.between?(0, grid.length - 1)
-
-          energised << [_x, _y]
-          next_cur << [[_x, _y], new_d] unless visited.include?([[_x, _y], new_d])
-          visited << [[_x, _y], new_d]
+        energised << [_x, _y]
+        state = [_x, _y, new_d]
+        unless visited.include?(state)
+          current.unshift(state)
+          visited << state
         end
       end
-
-      current = next_cur
     end
 
     max = [max, energised.length].max
 
-    start_x += 1 if edge % 2 == 0
-    start_y += 1 if edge % 2 == 1
+    start_y += 1 if edge % 2 == 0
+    start_x += 1 if edge % 2 == 1
   end
 end
 
