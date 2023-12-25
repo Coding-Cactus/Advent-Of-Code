96a8775c41d1ad597d449b2a5e972dfe91cc7b42
Coding-Cactus <codingcactus.cc@gmail.com>
2023-12-25 18:19:30 +0000

    :zap: day 24 use Z3

--- 2023/day24/part2.rb
+++ 2023/day24/part2.rb
@@ -1,96 +1,22 @@
-# takes a few hours, will rewrite at some point
+require "z3"
 
-require "set"
-require "matrix"
-require "bigdecimal"
+def solve(lines)
+  solver = Z3::Solver.new
 
-def diagonal(n)
-  sequences = []
+  lines[...3].each_with_index do |line, i|
+    pos, vel = line.split(" @ ")
+    xn, yn, zn    = pos.split(", ").map(&:to_i)
+    vxn, vyn, vzn = vel.split(", ").map(&:to_i)
 
-  0.upto(n) do |v1|
-    0.upto(n - v1) do |v2|
-      v3 = n - v1 - v2
-      sequences << [v1, v2, v3]
-      sequences << [v1, v2, -v3]   unless v3.zero?
-      sequences << [v1, -v2, v3]   unless v2.zero?
-      sequences << [v1, -v2, -v3]  unless v2.zero? || v3.zero?
-      sequences << [-v1, v2, v3]   unless v1.zero?
-      sequences << [-v1, v2, -v3]  unless v1.zero? || v3.zero?
-      sequences << [-v1, -v2, v3]  unless v1.zero? || v2.zero?
-      sequences << [-v1, -v2, -v3] unless v1.zero? || v2.zero? || v3.zero?
-    end
+    solver.assert(Z3.Int("x") + Z3.Int("vx") * Z3.Int("t#{i}") == xn + vxn * Z3.Int("t#{i}"))
+    solver.assert(Z3.Int("y") + Z3.Int("vy") * Z3.Int("t#{i}") == yn + vyn * Z3.Int("t#{i}"))
+    solver.assert(Z3.Int("z") + Z3.Int("vz") * Z3.Int("t#{i}") == zn + vzn * Z3.Int("t#{i}"))
   end
 
-  sequences
-end
-
-
-lines = File.foreach("input.txt").map(&:strip)
-
-hail = lines[...3].map do |line|
-  pos, vel = line.split(" @ ")
-  xn, yn, zn    = pos.split(", ").map(&:to_i)
-  vxn, vyn, vzn = vel.split(", ").map(&:to_i)
-
-  [xn, yn, zn, vxn, vyn, vzn]
-end
-
-hail0, hail1, hail2 = hail
-
-x0, y0, z0, vx0, vy0, vz0 = hail0
-x1, y1, z1, vx1, vy1, vz1 = hail1
-x2, y2, z2, vx2, vy2, vz2 = hail2
-
-diagonal_i = 0
-
-loop do
-  p diagonal_i
-  d = diagonal(diagonal_i)
-  diagonal_i += 1
-
-  d.each do |vx, vy, vz|
-    coeffs = Matrix[
-      [1, 0, 0, vx - vx0,        0],
-      [0, 1, 0, vy - vy0,        0],
-      [0, 0, 1, vz - vz0,        0],
-      [1, 0, 0,        0, vx - vx1],
-      [0, 1, 0,        0, vy - vy1]
-    ]
-
-    rhs = Matrix[
-      [x0],
-      [y0],
-      [z0],
-      [x1],
-      [y1]
-    ]
-
-    next if coeffs.determinant == 0
-
-    x, y, z, t0, t1 = (coeffs.inverse * rhs).each.to_a.map(&:to_i)
-
-    next if z + vz * t1 != z1 + vz1 * t1
-
-    coeffs = Matrix[
-      [1, 0, 0, vx - vx1,        0],
-      [0, 1, 0, vy - vy1,        0],
-      [0, 0, 1, vz - vz1,        0],
-      [1, 0, 0,        0, vx - vx2],
-      [0, 1, 0,        0, vy - vy2]
-    ]
-
-    rhs = Matrix[
-      [x1],
-      [y1],
-      [z1],
-      [x2],
-      [y2]
-    ]
-
-    next if coeffs.determinant == 0
-
-    _x, _y, _z, _t1, t2 = (coeffs.inverse * rhs).each.to_a.map(&:to_i)
-
-    return x + y + z if x == _x && y == _y && z == _z && t1 == _t1 && z + vz * t2 == z2 + vz2 * t2
+  if solver.satisfiable?
+    model = solver.model
+    model[Z3.Int("x")].to_i + model[Z3.Int("y")].to_i + model[Z3.Int("z")].to_i
+  else
+    "Didn't work :("
   end
 end
