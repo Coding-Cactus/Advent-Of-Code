d2a79655271dc7ff6554ba56e35defde92f4fb43
Coding-Cactus <codingcactus.cc@gmail.com>
2024-12-13 09:46:53 +0000

    :sparkles: day 13

---
+++ 2024/day13/part2.rb
@@ -0,0 +1,32 @@
+require "matrix"
+
+def solve(lines)
+  i = 0
+  total = 0
+  until i >= lines.length
+    x1 = lines[i].scan(/X\+(\d+)/).first.first.to_i
+    y1 = lines[i].scan(/Y\+(\d+)/).first.first.to_i
+    x2 = lines[i+1].scan(/X\+(\d+)/).first.first.to_i
+    y2 = lines[i+1].scan(/Y\+(\d+)/).first.first.to_i
+    x3 = lines[i+2].scan(/X=(\d+)/).first.first.to_i + 10000000000000
+    y3 = lines[i+2].scan(/Y=(\d+)/).first.first.to_i + 10000000000000
+    i += 4
+
+    coeffs = Matrix[
+      [x1, x2],
+      [y1, y2]
+    ]
+
+    next if coeffs.determinant.zero?
+
+    res = Matrix[
+      [x3],
+      [y3]
+    ]
+
+    x, y = (coeffs.inverse * res).each.to_a.map(&:to_f)
+    total += x * 3 + y if [x, y].all? { |n| n.to_i == n }
+  end
+
+  total.to_i
+end
