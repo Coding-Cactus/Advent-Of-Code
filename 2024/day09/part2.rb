7e4fa46e9d7316ea9e52005e69b058f6a88662e2
Coding-Cactus <codingcactus.cc@gmail.com>
2024-12-20 10:40:48 +0000

    :zap: remove unnecessary complication

--- 2024/day09/part2.rb
+++ 2024/day09/part2.rb
@@ -1,27 +1,3 @@
-def insert_gap(gaplen, gapptr, gaps)
-  x1, x2 = gapptr, gapptr + gaplen - 1
-  new_gap = [0, 0]
-  inserted = false
-
-  new_gaps = []
-  gaps.each do |gap|
-    len, ptr = gap
-    y1, y2 = ptr, ptr + len - 1
-    if (x1 <= y1 && x2 >= y1) || (y1 < x1 && y2 >= x1)
-      x1 = [x1, y1].min
-      x2 = [y2, x2].max
-
-      new_gaps << new_gap unless inserted
-      inserted = true
-    else
-      new_gaps << gap
-    end
-  end
-
-  new_gap[0], new_gap[1] = x2 - x1 + 1, x1
-  new_gaps
-end
-
 def solve(lines)
   gaps = []
   files = []
@@ -45,7 +21,6 @@ def solve(lines)
       next if gaplen < flen
 
       file[2] = gapptr
-      gaps = insert_gap(flen, fptr, gaps)
 
       gap[0] = gaplen - flen
       gap[1] = gapptr + flen
